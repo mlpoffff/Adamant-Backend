6321c57bcad31d727e1358f1caa34d444d3c02ee
maksik <123letsplay93@gmail.com>
2025-10-08 18:48:07 +0700

    FEAT(ADMIN): статьи

---
+++ app/Filament/Resources/Articles/Schemas/ArticleForm.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\Articles\Schemas;
+
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class ArticleForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')->label('Заголовок')
+                    ->required(),
+                TextInput::make('preview')->label('Краткое описание')
+                    ->required(),
+                RichEditor::make('content')->label('Статья')
+                    ->required()
+                    ->columnSpanFull(),
+            ]);
+    }
+}
--- app/Models/Article.php
+++ app/Models/Article.php
@@ -16,6 +16,7 @@ class Article extends Model
      */
     protected $fillable = [
         'title',
+        'preview',
         'content',
     ];
 
